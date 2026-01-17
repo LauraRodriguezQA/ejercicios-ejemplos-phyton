157a171907fd46d16c22ce927e4bfebfc863e205
Laura Rodrigue <rgmarialaura@gmail.com>
2026-01-17 15:10:41 +0100

    ejercicio pelicula

---
+++ ejercicios/2variablesejercicios.py
@@ -0,0 +1,20 @@
+#Cadena
+pelicula = "Monster Inc"
+director = "Pete Docter"
+#Numeros (int)
+año_lanzamiento = 2001
+genero = "Familiar"
+duracion = 93
+#Booleanos
+tiene_premios = True
+
+#Imprimir las variables
+print ("pelicula", pelicula)
+print ("director", director)
+print ("año lanzamiento", año_lanzamiento)
+print ("genero", genero)
+print ("duracion", duracion)
+print ("tiene premios?", tiene_premios)
+
+
+
