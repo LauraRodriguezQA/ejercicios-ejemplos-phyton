36f638ef0692bd5f35cad4747f9cc3c2fa69e5c2
Laura Rodrigue <rgmarialaura@gmail.com>
2026-01-19 21:41:55 +0100

    ejercicios

--- ejercicios/2variablesejercicios.py
+++ ejercicios/2variablesejercicios.py
@@ -3,7 +3,7 @@ pelicula = "Monster Inc"
 director = "Pete Docter"
 #Numeros (int)
 año_lanzamiento = 2001
-genero = "Familiar"
+genero = "Animacion"
 duracion = 93
 #Booleanos
 tiene_premios = True
@@ -13,8 +13,25 @@ print ("pelicula", pelicula)
 print ("director", director)
 print ("año lanzamiento", año_lanzamiento)
 print ("genero", genero)
-print ("duracion", duracion)
+print ("duracion en minutos", duracion)
 print ("tiene premios?", tiene_premios)
 
+#Cambiar variables a ingles
+#Cadena
+movie = "Monster Inc"
+director = "Pete Docter"
+#Numeros (int)
+año_lanzamiento = 2001
+genero = "Animation"
+duracion = 93
+#Booleanos
+tiene_premios = True
 
-
+# Mostrar datos de la película en inglés
+print("\nPelícula favorita (en inglés):")
+print("Movie:", Monster Inc)
+print("Director:", director)
+print("Year of release:", año)
+print("Genre:", genero)
+print("Duration (minutes):", duracion)
+print("Has awards?:", tiene_premios)
