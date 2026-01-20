485b5c3ff6e0b5e28a5a6044269bf2ea9e59876d
Laura Rodrigue <rgmarialaura@gmail.com>
2026-01-20 14:01:09 +0100

    ejercicio calculadora

---
+++ ejercicios/t3e1calculadora.py
@@ -0,0 +1,16 @@
+#Pedimos al usuario que introduzca los numeros
+num1 = float (input("Introduce el primero numero: "))
+num2 = float (input("Introduce el numero dos: "))
+
+#Realizar operaciones 
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+#Mostrar resultados
+print ("/nResultados de la calculadora: ")
+print ("Suma", suma)
+print ("Resta", resta)
+print ("Multiplicacion", multiplicacion)
+print ("Division", division)
