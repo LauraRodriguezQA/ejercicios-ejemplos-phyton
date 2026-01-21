5150843e5b3a29fe29d9cf6f5a90b27b5f68cfb2
Laura Rodrigue <rgmarialaura@gmail.com>
2026-01-21 14:11:07 +0100

    ejercicio correo electronico

---
+++ ejercicios/t4e2correoelectronico.py
@@ -0,0 +1,20 @@
+#Pedir al usuario que ingrese un correo electronico
+correo_electronico = input("Ingresar un correo electronico: ")
+
+#Mostrar la longitud del correo electronico
+longitud = len(correo_electronico)
+
+#Mostrar correo electronico en mayusculas
+correomayusculas = correo_electronico.upper()
+
+#Mostrar correo electronico en minusculas
+correominusculas = correo_electronico.lower()
+
+#Imprimir
+print ("Imprimir el correo electronico del usuario")
+print ("Longitud", longitud)
+print (correomayusculas)
+print (correominusculas)
+
+
+
