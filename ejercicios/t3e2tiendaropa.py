5a8f89df9bc71fed40eb0f7eede630535b857c74
Laura Rodrigue <rgmarialaura@gmail.com>
2026-01-21 13:12:26 +0100

    ejercicio tienda ropa

---
+++ ejercicios/t3e2tiendaropa.py
@@ -0,0 +1,33 @@
+#Precios
+precio_camiseta: 10
+precio_sudadera: 20.5
+precio_gorra: 5.5
+iva = 0.21  #IVA del 21%
+
+#Pedir cantidades al usuario
+cantidad_camisetas = int(input("Introduce la cantidad de camisetas: "))
+cantidad_sudaderas = int(input("Introduce la cantidad de sudaderas: "))
+cantidad_gorras = int(input("Introduce la cantidad de gorras: "))
+
+#Subtotal de la compra
+subtotal = (cantidad_camisetas * precio_camiseta +
+            cantidad_sudaderas * precio_sudadera +
+            cantidad_gorras * precio_gorra)
+
+#Total con IVA
+subtotal = subtotal * iva
+total_compra = subtotal + iva
+
+#Mostrar el total de la compra
+print("\nResumen de la compra: ")
+print("Subtotal: ", subtotal, "euros")
+print("IVA (21%):", iva, "euros")
+print("Total con IVA:", total_compra, "euros")
+
+
+
+
+
+
+
+
