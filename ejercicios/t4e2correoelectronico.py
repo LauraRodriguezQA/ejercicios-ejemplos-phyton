9522986ba325b36d2c14b4e1017350bdfd1dba39
Laura Rodrigue <rgmarialaura@gmail.com>
2026-01-21 14:17:22 +0100

    correcciones t4e2correo

--- ejercicios/t4e2correoelectronico.py
+++ ejercicios/t4e2correoelectronico.py
@@ -3,18 +3,15 @@ correo_electronico = input("Ingresar un correo electronico: ")
 
 #Mostrar la longitud del correo electronico
 longitud = len(correo_electronico)
+print ("La longitud del correo electronico es: ", longitud)
 
 #Mostrar correo electronico en mayusculas
 correomayusculas = correo_electronico.upper()
+print ("Correo electronico en mayusculas", correomayusculas)
 
 #Mostrar correo electronico en minusculas
 correominusculas = correo_electronico.lower()
-
-#Imprimir
-print ("Imprimir el correo electronico del usuario")
-print ("Longitud", longitud)
-print (correomayusculas)
-print (correominusculas)
+print ("Correo electronico en minusculas", correominusculas)
 
 
 
