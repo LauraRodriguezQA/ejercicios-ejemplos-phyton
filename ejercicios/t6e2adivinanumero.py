5248da6edefd34773b237708086c614d8adcde00
Laura Rodrigue <rgmarialaura@gmail.com>
2026-01-22 21:20:43 +0100

    ejercicio adivina el numero

---
+++ ejercicios/t6e2adivinanumero.py
@@ -0,0 +1,28 @@
+#Funcion para establecer el numero ganador
+def adivinar_numero(numero):
+    numero_ganador = 4 #numero ganador
+    mensaje = ""
+
+#Asignar el mensaje
+
+    if numero == numero_ganador:
+     mensaje = "¡Haz ganado! El numero es el correcto."
+    else:
+       mensaje = "Lo siento, perdiste. El numero correcto era el 4."
+       
+       return mensaje 
+    
+    #Funcion principal
+def adivinanza_numero():
+   
+   #Pedir al usuario que intruzca un numero del 1 al 10 
+   
+ numero_usuario = int(input("Introducir un numero del 1 al 10: "))
+
+#Mostrar el mensaje segun el numero elegido
+ mensaje = adivinar_numero(numero_usuario)
+ print (mensaje)
+
+#Llamada funcion principal 
+adivinanza_numero()
+
